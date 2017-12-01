ca948a1f636bf7a75130f7f12cf4269712c9db46
ManuGil-PC <madtriangl3@gmail.com>
2017-12-01 22:58:19 +0000

    texture_frame_buffer

--- A_1_Solution/src/core/util/texture/Texture.cpp
+++ A_1_Solution/src/core/util/texture/Texture.cpp
@@ -4,22 +4,20 @@
 
 #include <std_image\stb_image.h>
 
-Texture::Texture(GLenum texture_target, const std::string & file_name) : texture_target(texture_target), file_name(file_name){
-	texture_id = load();
+Texture::Texture(GLenum texture_target, const std::string & file_name, int num_of_textures) : texture_target(texture_target), file_name(file_name), num_of_textures(num_of_textures) {
+	texture_id = new GLuint[num_of_textures];
+	
 }
 
 GLint Texture::load() {
-	int width, height, num_of_components;
-	unsigned char* image_data = stbi_load(file_name.c_str(), &width, &height, &num_of_components, STBI_rgb_alpha);
+	image_data = stbi_load(file_name.c_str(), &width, &height, &num_of_components, STBI_rgb_alpha);
 
 	if (image_data == nullptr) {
 		std::cout << "Texture loading failed: " << file_name.c_str() << std::endl;
-		return -1;
 	}
-	GLuint id;
-	glGenTextures(1, &id);
+	glGenTextures(num_of_textures, texture_id);
 	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(texture_target, id);
+	glBindTexture(texture_target, *texture_id);
 
 	glTexParameteri(texture_target, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(texture_target, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -32,12 +30,25 @@ GLint Texture::load() {
 	glBindTexture(texture_target, 0);
 
 	stbi_image_free(image_data);
-	return id;
+	return 1;
 }
 
-void Texture::bind(int texture_unit) {
+void Texture::init(unsigned char** data, GLfloat* filter) {
+	glGenTextures(num_of_textures, texture_id);
+	for (int i = 0; i < num_of_textures; i++) {
+
+		glBindTexture(texture_target, texture_id[i]);
+
+		glTexParameterf(texture_target, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameterf(texture_target, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		glTexImage2D(texture_target, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, image_data);
+	}
+}
+
+void Texture::bind(int texture_unit) const {
 	glActiveTexture(GL_TEXTURE0 + texture_unit);
-	glBindTexture(texture_target, texture_id);
+	glBindTexture(texture_target, *texture_id);
 }
 
 void Texture::unbind() {
@@ -50,6 +61,12 @@ int Texture::get_unit() {
 std::string Texture::get_file_name() {
 	return this->file_name;
 }
+
+void Texture::bindAsRenderTarget() {
+
+}
+
 Texture::~Texture() {
-	glDeleteTextures(1, &texture_id);
+	if (*texture_id) glDeleteTextures(num_of_textures, texture_id);
+	if (texture_id) delete texture_id;
 }
--- A_1_Solution/src/core/util/texture/Texture.h
+++ A_1_Solution/src/core/util/texture/Texture.h
@@ -7,19 +7,26 @@ const std::string TEXTURE_WHITE_PIXEL = "textures/empty_pixel.jpg";
 
 class Texture {
 public:
-	Texture(GLenum texture_target=GL_TEXTURE_2D, const std::string &file_name=TEXTURE_WHITE_PIXEL);
+	Texture(GLenum texture_target=GL_TEXTURE_2D, const std::string &file_name=TEXTURE_WHITE_PIXEL, int num_of_textures=1);
 
 	GLint load();
-	void bind(int texture_unit);
+	void init(unsigned char** data, GLfloat* filter);
+	void bind(int texture_unit) const;
 	void unbind();
 	int get_unit();
 	std::string get_file_name();
 	~Texture();
 
+	void bindAsRenderTarget();
+
 private:
 	int unit = 0;
 
-	GLuint texture_id;
+	GLuint* texture_id;
+
+	int width, height, num_of_components;
+	unsigned char* image_data;
+	int num_of_textures;
 	GLenum texture_target;
 	std::string file_name;
 };
